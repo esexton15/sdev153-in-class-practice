86f486dcbe414f097ac6c3009970865cc449733f
Eric Sexton <foopis23@gmail.com>
2026-02-09 14:30:04 -0500

    starting point

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tunnel from 'astro-tunnel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		server: {
+			allowedHosts: true,
+		},
+		preview: {
+			allowedHosts: true,
+		},
+	},
+	integrations: [tunnel()],
+});
