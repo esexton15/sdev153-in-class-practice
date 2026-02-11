472c9ab727e36de5973696725068e323675e8e1f
Eric Sexton <esexton15@ivytech.edu>
2026-02-11 21:13:18 +0000

    added sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tunnel from 'astro-tunnel';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	base: "/sdev-153-in-class-practice",
-	vite: {
-		server: {
-			allowedHosts: [
-				"vacuous-tameika-adsorptively.ngrok-free.dev",
-			],
-		},
-	},
-	integrations: [tunnel()],
+    base: "/sdev-153-in-class-practice",
+    vite: {
+        server: {
+            allowedHosts: [
+                "vacuous-tameika-adsorptively.ngrok-free.dev",
+            ],
+        },
+    },
+    integrations: [tunnel(), sitemap()],
 });
